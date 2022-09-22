60806c2e30ebfca1f033fac8ee067b2247697104
Kirill Mantiuk <kerchuk47@gmail.com>
2022-09-22 16:55:47 +0300

    First Commit
    First Commit

---
+++ Magic Ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Magic Ball
+//
+//  Created by Kirill Mantiuk on 22.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var text: UILabel!
+    
+    @IBAction func askButton(_ sender: UIButton) {
+        
+        let answer = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", " Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+        
+        text.text = answer [Int.random(in: 0 ... 19)]
+        
+        
+    }
+}
+
